262525504cdb9a31b5907bb2e28e7f4dad33228f
sorteggideifiori <101125532+sorteggi@users.noreply.github.com>
2024-06-05 08:35:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -22,10 +22,10 @@ const firestore = firebase.firestore();
 const servers = {
   iceServers: [
     {
-      urls: "stun:stun.l.google.com:19302"
+      urls: 'stun:stun.l.google.com:19302'
     },
     { 
-      urls: "stun:global.stun.twilio.com:3478?transport=udp
+      urls: 'stun:global.stun.twilio.com:3478?transport=udp'
     },
 ],
   iceCandidatePoolSize: 10,
